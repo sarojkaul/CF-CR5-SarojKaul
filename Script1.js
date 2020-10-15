101fcd76393e23a5102e15eff2432ce0af7f0aa1
saroj kaul <sarojmahi73@gmail.com>
2020-10-15 13:43:13 +0200

    adding

--- Script1.js
+++ Script1.js
@@ -2,36 +2,36 @@
 let data = JSON.parse(dataMovies);
 for (i = 0; i < data.length; i++) {
     $(".addImages").append(`
- <div class="card mb-5" style="max-width: 450px;">
-<div class="row">
+ <div class="card mb-5 mr-5 ml-5 mb-0" style="max-width: 450px;">
+<div class="row mb-0">
 <div class="col-6 col-lg-6 col-xl-6">
 <img src="${data[i].image}" alt="moviesposter" class="images">
 </div>
-<div class="col-6 col-lg-6 col-xl-6">
-</div>
+<div class="col-6 col-lg-6 col-xl-6 mb-0">
+<div>
 <h4>${data[i].name}</h4>
 <p class="card-text">${data[i].description}</p>
 </div>
-<span class="row">
-<p class="liketext">Likes</p>
+</div>
+
+</div>
+<span class ="row sidetext">
 <button type="button class="likes">Likes</button>
 <div class="circle">
 <p id="${i}" class="likes">${data[i].likes}</p>
 </div>
 </span>
+
 </div>
 </div>`)
 
 };
-$(document).ready(function () {
-    $(".likes").on("click", function () {
+$(document).ready(function() {
+    $(".likes").on("click", function() {
 
         var index = $(this).parent().attr("id");
         data[index].likes += 1;
-       
+
         $(this).parent().find(".likes").html(data[index].likes);
-        
     })
-   
-
-
+})
