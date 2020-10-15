55f0fd39b34f9177b294f74bf1a4ecb7d49455de
saroj kaul <sarojmahi73@gmail.com>
2020-10-15 19:13:23 +0200

    adding

--- Script1.js
+++ Script1.js
@@ -2,36 +2,33 @@
 let data = JSON.parse(dataMovies);
 for (i = 0; i < data.length; i++) {
     $(".addImages").append(`
- <div class="card mb-5 mr-5 ml-5 mb-0" style="max-width: 450px;">
-<div class="row mb-0">
-<div class="col-6 col-lg-6 col-xl-6">
+ <div class="card mb-5 mr-5 ml-5" style="max-width: 450px;">
+ <div class="row">
+<div class="col-5 col-lg-6 col-xl-6">
 <img src="${data[i].image}" alt="moviesposter" class="images">
 </div>
-<div class="col-6 col-lg-6 col-xl-6 mb-0">
-<div>
+<div class="col-7 col-lg-6 col-xl-6 textmovie" id="pa">
 <h4>${data[i].name}</h4>
 <p class="card-text">${data[i].description}</p>
 </div>
-</div>
-
-</div>
 <span class ="row sidetext">
-<button type="button class="likes">Likes</button>
-<div class="circle">
-<p id="${i}" class="likes">${data[i].likes}</p>
-</div>
-</span>
+<p class = "liketext">Likes</p>
+<button class="fa fa-thumbs-up btn"></button>
+<p id="${i}" class="likes1">${data[i].likes}</p>
 
+</span>
+</div>
 </div>
 </div>`)
 
 };
 $(document).ready(function() {
-    $(".likes").on("click", function() {
+    $(".btn").on("click", function() {
 
-        var index = $(this).parent().attr("id");
-        data[index].likes += 1;
+        let index = $(this).parent().find(".likes1").attr("id");
+        data[index].likes++;
 
-        $(this).parent().find(".likes").html(data[index].likes);
+        $(this).parent().find(".likes1").html(data[index].likes);
+
     })
 })
